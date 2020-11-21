fc7983c8e7b3eac7182a5b92dd9feda76fe0f1ca
s70160 <67191334+s70160@users.noreply.github.com>
2020-11-21 21:57:55 +0800

    Update driver_monitor.py

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -14,9 +14,9 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 35.  # passive wheel touch total timeout
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
+_AWARENESS_TIME = 336000.  # passive wheel touch total timeout
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 35.
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 30.
 _DISTRACTED_TIME = 11.
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
